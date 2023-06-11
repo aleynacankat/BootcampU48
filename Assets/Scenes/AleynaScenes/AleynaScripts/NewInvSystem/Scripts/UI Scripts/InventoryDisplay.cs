6521f3d03636b49475304dffe7f9de01fee08b6a
Aleyna Cankat <aleynacankat0@gmail.com>
2023-06-11 19:06:06 +0300

    updateSlot

--- Assets/Scenes/AleynaScenes/AleynaScripts/NewInvSystem/Scripts/UI Scripts/InventoryDisplay.cs
+++ Assets/Scenes/AleynaScenes/AleynaScripts/NewInvSystem/Scripts/UI Scripts/InventoryDisplay.cs
@@ -10,13 +10,26 @@ public abstract class InventoryDisplay : MonoBehaviour
     public InventorySystem InventorySystem => inventorySystem;
     public Dictionary<InventorySlot_UI, InventorySlot> SlotDictionary => slotDictionary;
 
+    protected virtual void Start()
+    {
+
+    }
+
     public abstract void AssignSlot(InventorySystem invToDisplay);
 
     protected virtual void UpdateSlot(InventorySlot updatedSlot)
     {
         foreach (var slot in SlotDictionary)
         {
-
+            if  (slot.Value == updatedSlot) //Slot value - the "under the hood" inventory slot.
+            {
+                slot.Key.UpdateUISlot(updatedSlot); //Slot key - the UI representation of the value
+            }
         }
+    }
+
+    public void SlotClicked(InventorySlot_UI clickedSlot)
+    {
+        Debug.Log("Slot clicked");
     }
 }
