341f9c2107d3411fbea04fcf2afef112a275b444
Aleyna Cankat <aleynacankat0@gmail.com>
2023-06-11 15:38:58 +0300

    updateBug

--- Assets/PlayerMovementInv.cs
+++ Assets/PlayerMovementInv.cs
@@ -9,7 +9,7 @@ public class PlayerMovementInv : MonoBehaviour
     private CharacterController controller;
     private Vector3 playerVelocity;
     private bool groundedPlayer;
-    private float playerSpeed = 2.0f;
+    public float playerSpeed = 2.0f;
     private float gravityValue = -9.81f;
 
     private PlayerControlsInv playerControlsInv;
