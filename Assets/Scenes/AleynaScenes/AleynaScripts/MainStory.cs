215a8b7af2d6e34fa9d6a8647705b08337daff38
Aleyna Cankat <aleynacankat0@gmail.com>
2023-06-16 00:04:07 +0300

    update

---
+++ Assets/Scenes/AleynaScenes/AleynaScripts/MainStory.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainStory : MonoBehaviour
+{
+    void OnEnable()
+    {
+        //Only specifiying the sceneName or sceneBuildIndex will load the Scene with the Single mode
+        SceneManager.LoadScene("Intro", LoadSceneMode.Single);
+    }
+
+}
