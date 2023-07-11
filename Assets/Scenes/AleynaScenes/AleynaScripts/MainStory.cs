c651bf343a66166124d0595684b2956a62f187ae
Emirhan Topal <emirhantopal03@hotmail.com>
2023-07-12 00:13:40 +0300

    add5678676678

--- Assets/Scenes/AleynaScenes/AleynaScripts/MainStory.cs
+++ Assets/Scenes/AleynaScenes/AleynaScripts/MainStory.cs
@@ -5,11 +5,10 @@ using UnityEngine.SceneManagement;
 
 public class MainStory : MonoBehaviour
 {
-    public string _name;
     void OnEnable()
     {
         //Only specifiying the sceneName or sceneBuildIndex will load the Scene with the Single mode
-        SceneManager.LoadScene(_name, LoadSceneMode.Single);
+        SceneManager.LoadScene("3HomeMorningA", LoadSceneMode.Single);
     }
 
 }
