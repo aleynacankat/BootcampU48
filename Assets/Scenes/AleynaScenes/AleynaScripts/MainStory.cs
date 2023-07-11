25262f54a3c3708b42812cc73d4497fab68d13af
Emirhan Topal <emirhantopal03@hotmail.com>
2023-07-11 23:34:58 +0300

    add5

--- Assets/Scenes/AleynaScenes/AleynaScripts/MainStory.cs
+++ Assets/Scenes/AleynaScenes/AleynaScripts/MainStory.cs
@@ -5,10 +5,11 @@ using UnityEngine.SceneManagement;
 
 public class MainStory : MonoBehaviour
 {
+    public string _nameScene;
     void OnEnable()
     {
         //Only specifiying the sceneName or sceneBuildIndex will load the Scene with the Single mode
-        SceneManager.LoadScene("2Base", LoadSceneMode.Single);
+        SceneManager.LoadScene(_nameScene, LoadSceneMode.Single);
     }
 
 }
